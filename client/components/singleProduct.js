e84280dd070bda7d2d2e1057584b001c9d18c08a
Ashlynn Mesidor <ashlynnmesidor@yahoo.com>
2019-03-03 15:36:36 -0500

    feat(component/single product): storing cart on local storage

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -4,9 +4,26 @@ import {getOneProductThunk} from '../store/allProducts'
 import {Link} from 'react-router-dom'
 
 class SingleProduct extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: '1'}
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
   componentDidMount() {
     const productId = this.props.match.params.productId
     this.props.loadOneDuck(productId)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    localStorage.setItem(this.props.currentProduct.name, this.state.value)
+    console.log(localStorage)
   }
 
   render() {
@@ -18,15 +35,16 @@ class SingleProduct extends React.Component {
         <img src={currentProduct.imageUrl} width="100px" />
         <p>${currentProduct.price}</p>
         <p>{currentProduct.description}</p>
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
-        <button type="submit">Add to cart</button>
-        <p />
+        <form onSubmit={this.handleSubmit}>
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <button type="submit">Add to cart</button>
+        </form>
         <Link to="/products">Back to all Producks</Link>
       </div>
     )
@@ -34,7 +52,10 @@ class SingleProduct extends React.Component {
 }
 
 const mapStateToProps = state => {
-  return {currentProduct: state.products.currentProduct}
+  return {
+    currentProduct: state.products.currentProduct,
+    user: state.user
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
