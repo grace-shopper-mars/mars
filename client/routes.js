95d9c0ada358ff0c8d013b1c43b54749629dd115
Alexandra Beautyman <alexandra.beautyman@gmail.com>
2019-03-05 16:19:31 -0500

    feat(user cart): user can edit quantity of items in cart, closes #94

--- client/routes.js
+++ client/routes.js
@@ -48,6 +48,7 @@ class Routes extends Component {
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
+            <Route component={AllProduct} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
